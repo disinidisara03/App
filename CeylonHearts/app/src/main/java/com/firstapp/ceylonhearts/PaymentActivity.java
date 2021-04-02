8cc5687abcdfd2e74e1cbfc70ba4892c0b7475ab
Disini Disara <gmddrathnayaka@students.nsbm.lk>
2021-04-02 14:58:41 +0530

    payment message
    Co-Authored-By: Jabbar Azhar Mujeeb <67254298+azharmujeeb@users.noreply.github.com>

--- CeylonHearts/app/src/main/java/com/firstapp/ceylonhearts/PaymentActivity.java
+++ CeylonHearts/app/src/main/java/com/firstapp/ceylonhearts/PaymentActivity.java
@@ -3,12 +3,31 @@ package com.firstapp.ceylonhearts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PaymentActivity extends AppCompatActivity {
+
+    Button donate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        donate = (Button) findViewById(R.id.btn_donate);
+
+        donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Messagetoshow();
+            }
+        });
+    }
+
+    private void Messagetoshow() {
+        Toast.makeText(this, "Thank you for making a difference.Successfully Donated", Toast.LENGTH_SHORT).show();
+
     }
 }
